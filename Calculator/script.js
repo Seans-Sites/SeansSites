2f9427dc1d42dfdda184aabd090ea0bf1d4725ae
Seans-Sites <seanscoolsites@gmail.com>
2024-10-23 22:44:36 -0400

    Create script.js

---
+++ Calculator/script.js
@@ -0,0 +1,13 @@
+console.log('hello world');
+
+// Hello
+
+document.getElementById("i1").textContent = "1";
+document.getElementById("i2").textContent = "2";
+document.getElementById("i3").textContent = "3";
+document.getElementById("i4").textContent = "4";
+document.getElementById("i5").textContent = "5";
+document.getElementById("i6").textContent = "6";
+document.getElementById("i7").textContent = "7";
+document.getElementById("i8").textContent = "8";
+document.getElementById("i9").textContent = "9";
